375fadcefdc2f96a9a04b2d4a083982e33f2033c
AWEI0407 <121768425+AWEI0407@users.noreply.github.com>
2023-06-25 13:28:38 +0800

    更新

---
+++ src/main/java/cn/edu/whut/sept/zuul/BackCommand.java
@@ -0,0 +1,28 @@
+package ZuulCommands;
+
+import Commands.ACommand;
+import Character.Player;
+import Game.AGame;
+/**
+* 此类是back命令的实现类
+*/
+public class BackCommand extends ACommand{
+    public BackCommand(String firstWord, String secondWord, String thirdWord)
+    {
+        super(firstWord, secondWord, thirdWord);
+    }
+
+    public BackCommand() {}
+
+    @Override
+    /**
+     * back的执行方法，通过player对象调用具体方法
+     * 
+     */
+    public boolean execute(Player player)
+    {
+        player.back();
+        return false;
+    }
+}
+}
