212bf6c0a99c489accd0c2d6969c259467ba0999
AWEI0407 <121768425+AWEI0407@users.noreply.github.com>
2023-06-25 12:41:43 +0800

    主类重写

--- src/main/java/cn/edu/whut/sept/zuul/Game.java
+++ src/main/java/cn/edu/whut/sept/zuul/Game.java
@@ -1,92 +1,109 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import Character.Player;
+import GUI.GUI;
+
 /**
- * 该类是“World-of-Zuul”应用程序的主类。
- * 《World of Zuul》是一款简单的文本冒险游戏。用户可以在一些房间组成的迷宫中探险。
- * 你们可以通过扩展该游戏的功能使它更有趣!.
+ * 这个类是游戏类扩展到AGame
+ * @author duoduo
  *
- * 如果想开始执行这个游戏，用户需要创建Game类的一个实例并调用“play”方法。
- *
- * Game类的实例将创建并初始化所有其他类:它创建所有房间，并将它们连接成迷宫；它创建解析器
- * 接收用户输入，并将用户输入转换成命令后开始运行游戏。
- *
- * @author  Michael Kölling and David J. Barnes
- * @version 1.0
  */
-package cn.edu.whut.sept.zuul;
-
-public class Game
+public class Game extends AGame
 {
-    private Parser parser;
-    private Room currentRoom;
-
     public Game()
     {
-        createRooms();
-        parser = new Parser();
+    	super(new ZuulCommands.CommandWords());
     }
-
-    private void createRooms()
+	
+    /**
+     * 创建房间
+     * 连接房间
+     **/
+    @Override
+    protected void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room outside, theatre, pub, lab, office;
+        Room cafe, garden, store, restaurant, toilet,TProom;
+        this._rooms = new ArrayList<>();
 
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-
-        // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
-
-        theater.setExit("west", outside);
-
-        pub.setExit("east", outside);
-
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-
-        office.setExit("west", lab);
-
-        currentRoom = outside;  // start game outside
+        // 创建房间，房间共11个
+        outside = new Room(Game._messages.getString("outside"));
+        this._rooms.add(outside);
+        theatre = new Room(Game._messages.getString("lecture"));
+        this._rooms.add(theatre);
+        pub = new Room(Game._messages.getString("pub"));
+        this._rooms.add(pub);
+        lab = new Room(Game._messages.getString("lab"));
+        this._rooms.add(lab);
+        office = new Room(Game._messages.getString("admin"));
+        this._rooms.add(office);
+        cafe = new Room(Game._messages.getString("cafe"));
+        this._rooms.add(cafe);
+        garden = new Room(Game._messages.getString("garden"));
+        this._rooms.add(garden);
+        store = new Room(Game._messages.getString("store"));
+        this._rooms.add(store);
+        restaurant = new Room(Game._messages.getString("restaurant"));
+        this._rooms.add(restaurant);
+        toilet = new Room(Game._messages.getString("toilet"));
+        this._rooms.add(toilet);
+        TProom = new Room(Game._messages.getString("TProom"));
+        this._rooms.add(TProom);
+       
+        // 设计一个游戏地图并且以该地图来构建房间链接关系以及在房间里放置对应的物品（包含物品名称和物品重量）
+        outside.setExits(garden, theatre, lab, pub);
+        outside.addItem(Game._messages.getString("notebook"), 2);
+        theatre.setExits(TProom, cafe, null, outside);
+        theatre.addItem(Game._messages.getString("rock"), 2);
+        pub.setExits(null, outside, restaurant, null);
+        pub.addItem(Game._messages.getString("beer"), 2);
+        lab.setExits(outside, office, store, restaurant);
+        lab.addItem(Game._messages.getString("laptop"), 5);
+        office.setExits(null, null, null, lab);
+        office.addItem(Game._messages.getString("pen"), 1);
+        
+        cafe.setExits(null, null, null, theatre);
+        cafe.addItem(Game._messages.getString("cafe"), 1);
+        cafe.addItem(Game._messages.getString("tea"), 1);
+        garden.setExits(null, null, outside, null);
+        garden.addItem(Game._messages.getString("stick"), 2);
+        garden.addItem(Game._messages.getString("rabbit"), 1);
+        store.setExits(lab, null, null, toilet);
+        store.addItem(Game._messages.getString("candy"), 1);
+        restaurant.setExits(pub, lab, toilet, null);
+        restaurant.addItem(Game._messages.getString("food"), 2);
+        toilet.setExits(restaurant, store, null, null);
+        toilet.addItem(Game._messages.getString("paper"), 1);
+        TProom.setExits(pub,office,store,cafe);
+        //设置出现在房间里的角色信息
+        lab.addCharacter(new Player(Game._messages.getString("Gandalf"), lab));
+        pub.addCharacter(new Player(Game._messages.getString("Aragorn"), pub));
+        toilet.addCharacter(new Player(Game._messages.getString("Legolas"), toilet));
+        garden.addCharacter(new Player(Game._messages.getString("Gimly"), garden));
+        cafe.addCharacter(new Player(Game._messages.getString("Frodo"), cafe));
+        cafe.addCharacter(new Player(Game._messages.getString("Sam"), cafe));
+        setRooms(this._rooms);
+        this._player = new Player(Game._messages.getString("me"), outside);
+        setPlayer(this._player);
     }
-
-    public void play()
+    
+    /**
+     * get the welcome strings
+     */
+    @Override
+    protected List<String> getWelcomeStrings()
     {
-        printWelcome();
-
-        // Enter the main command loop.  Here we repeatedly read commands and
-        // execute them until the game is over.
-
-        boolean finished = false;
-        while (! finished) {
-            Command command = parser.getCommand();
-            if(command == null) {
-                System.out.println("I don't understand...");
-            } else {
-                finished = command.execute(this);
-            }
-        }
-
-        System.out.println("Thank you for playing.  Good bye.");
-    }
-
-    private void printWelcome()
-    {
-        System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type 'help' if you need help.");
-        System.out.println();
-        System.out.println(currentRoom.getLongDescription());
-    }
-
-    public Room getCurrentRoom() {
-        return currentRoom;
-    }
-
-    public void setCurrentRoom(Room room){
-        this.currentRoom = room;
+    	List<String> rv = new LinkedList<>();
+    	rv.add("");
+    	rv.add(Game._messages.getString("welcome"));
+    	rv.add(Game._messages.getString("zuul")); 
+    	rv.add(Game._messages.getString("getHelp"));
+    	rv.add("");
+    	rv.addAll(getPlayer().getDetails());
+    	return rv;
     }
 }
