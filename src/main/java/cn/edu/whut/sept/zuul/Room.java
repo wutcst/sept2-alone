f8040afe0753139677f7b49078df4a4f23542fcd
AWEI0407 <121768425+AWEI0407@users.noreply.github.com>
2023-06-25 13:25:23 +0800

    更新类

--- src/main/java/cn/edu/whut/sept/zuul/Room.java
+++ src/main/java/cn/edu/whut/sept/zuul/Room.java
@@ -1,49 +1,199 @@
-package cn.edu.whut.sept.zuul;
+package Game;
 
-import java.util.Set;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+import Game.AGame;
+import Item.Item;
+import Character.Player;
+import ZuulCommands.BadExitException;
+/**
+ * 这个类是游戏的房间类
+ * @author duoduo
+ *
+ */
 public class Room
 {
-    private String description;
-    private HashMap<String, Room> exits;        // stores exits of this room.
-
-    public Room(String description)
+    // 房间描述（名称）
+    final public String _description;
+    // 房间出口
+    final private Map<String, Room> _exits;
+    // 房间内物品的地图
+    private HashMap<String, Item> _itemsPresent;
+    // 人物地图
+    final private HashMap<String, Player> _characters;
+    //构造新的哈希表来储存房间里的物品和角色
+    public Room(String description) 
     {
-        this.description = description;
-        exits = new HashMap<>();
+        this._description = description;
+        this._itemsPresent = new HashMap<>();
+        this._characters = new HashMap<>();
+        this._exits = new HashMap<>();
     }
-
-
-    public void setExit(String direction, Room neighbor)
+     //设置四个方向的出口
+    public void setExits(Room north, Room east, Room south, Room west) 
     {
-        exits.put(direction, neighbor);
+        try
+        {
+    	    setExit(AGame._messages.getString("north"), north);
+    	    setExit(AGame._messages.getString("east"), east);
+    	    setExit(AGame._messages.getString("south"), south);
+    	    setExit(AGame._messages.getString("west"), west);
+    	}
+        catch (BadExitException e)//错误则转向ZuulCommand类中的控制错误的异常退出类
+        {
+    	    e.printStackTrace();
+    	    System.exit(1);
+    	}
     }
-
-    public String getShortDescription()
+    //通过BadExitException来实现
+    public void setExit(String direction, Room room) throws BadExitException
     {
-        return description;
+    	if (room == null)
+    	    return;
+        if (direction == null)
+            throw new BadExitException(direction, room);
+        this._exits.put(direction, room);
     }
-
-    public String getLongDescription()
-    {
-        return "You are " + description + ".\n" + getExitString();
-    }
-
-    private String getExitString()
-    {
-        String returnString = "Exits:";
-        Set<String> keys = exits.keySet();
-        for(String exit : keys) {
-            returnString += " " + exit;
-        }
-        return returnString;
-    }
-
+    //public Room getRoom(String direction)
+   
+    /**
+    *获取出口
+    */
     public Room getExit(String direction)
     {
-        return exits.get(direction);
+    	return this._exits.get(direction);
+    }
+   /**
+    * 获取房间的描述
+    */
+    public String getDescription()
+    {
+        return this._description;
+    }
+    /**
+     * 设置物品名字和物品重量
+     */
+    public void addItem(String name, int weight)
+    {
+    	Item item = new Item(name, weight);
+   		this._itemsPresent.put(name, item);
+    }
+    /**
+     * 将物品信息加入哈希表中
+     */
+    public void addItem(String name, Item item)
+    {
+    	if (item != null)
+    	{
+    		this._itemsPresent.put(name, item);
+    	}
+    }
+    /**
+     * 获取物品的数量
+     */
+    public int getNumberItems()
+    {
+    	return this._itemsPresent.size();
+    }
+    /**
+     * 获取物品
+     */
+    public Item getItem(String item)
+    {
+    	return this._itemsPresent.get(item);
+    }
+    /**
+     * 哈希表中所有的物品
+     */
+    public HashMap<String, Item> getAllItems()
+    {
+    	return this._itemsPresent;
+    }
+    /**
+     * 从哈希表中移除物品
+     */
+    public Item removeItem(String description)
+    {
+    	if (this._itemsPresent.containsKey(description))
+    		return this._itemsPresent.remove(description);
+    	else
+    	{
+    		AGame._out.println(description + " " + AGame._messages.getString("room"));
+    		return null;
+    	}
+    }
+    /**
+     *判断是否包含物品
+     */
+    public boolean containsItem(String description)
+    {
+    	return this._itemsPresent.containsKey(description);
+    }
+    /**
+     * 添加角色
+     */
+    public void addCharacter(Player character)
+    {
+    	this._characters.put(character.getName(), character);
+    }
+    /**
+     * 获取角色信息
+     */
+    public Player getCharacter(String name)
+    {
+    	return this._characters.get(name);
+    }
+    /**
+     * 移除角色
+     */
+    public void removeCharacter(String name)
+    {
+    	if (name != null)
+    		this._characters.remove(name);
+    }
+    /**
+     * 判断是否有角色
+     */
+    public boolean hasCharacter(String character)
+    {
+    	return this._characters.containsKey(character);
+    }
+    /**
+     * 哈希表中的角色
+     */
+    public HashMap<String, Player> getListCharacters()
+    {
+    	return this._characters;
+    }
+    /**
+     * 物品和角色的详细信息
+     */
+    public List<String> getDetails()
+    {   	
+    	List<String> details = new LinkedList<>();
+    	
+    	details.add(AGame._messages.getString("in") + " " + getDescription());
+        String tmp = AGame._messages.getString("exits") + ": ";
+        tmp = this._exits.keySet().stream()
+                .map((dir) -> dir + ' ')
+                .reduce(tmp, String::concat);
+        details.add(tmp);
+        
+        tmp = AGame._messages.getString("items") + ": ";
+        tmp = this._itemsPresent.keySet().stream()
+                .map((desc) -> desc + '(' + this._itemsPresent.get(desc).getWeight() + ')')
+                .reduce(tmp, String::concat);
+        details.add(tmp);
+        
+        tmp = AGame._messages.getString("characters") + ": ";
+        tmp = this._characters.keySet().stream()
+        		.map((desc) -> desc + ' ')
+        		.reduce(tmp, String::concat);    
+        details.add(tmp);
+        
+        return details;
     }
 }
-
-
