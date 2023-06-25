6eeb7d40738712800cf0ebad87575bb2ebc8cf6f
AWEI0407 <121768425+AWEI0407@users.noreply.github.com>
2023-06-25 12:47:00 +0800

    测试

--- src/main/java/cn/edu/whut/sept/zuul/Parser.java
+++ src/main/java/cn/edu/whut/sept/zuul/Parser.java
@@ -1,42 +1,55 @@
 package cn.edu.whut.sept.zuul;
 
+/**
+*“祖尔世界”是一款非常简单的基于文本的冒险游戏。
+ * 
+* 此解析器读取用户输入并尝试将其解释为“冒险”
+*命令。每次调用它时，它都会从终端读取一行，然后
+* 尝试将该行解释为两个单词的命令。它返回命令
+* 作为类命令的对象。
+ *
+* 解析器有一组已知的命令字。它检查用户输入
+* 已知命令，如果输入不是已知命令之一，则
+* 返回标记为未知命令的命令对象。
+**/
 import java.util.Scanner;
-
-
+/*创建parser对获取到的命令关键词进行解析*/
 public class Parser
 {
-    private CommandWords commands;  // holds all valid command words
-    private Scanner reader;         // source of command input
+    private CommandWords commands;
+    private Scanner reader;
 
     public Parser()
     {
         commands = new CommandWords();
         reader = new Scanner(System.in);
     }
-
+      //变量初始化
     public Command getCommand()
     {
-        String inputLine;   // will hold the full input line
+        String inputLine;
         String word1 = null;
         String word2 = null;
 
-        System.out.print("> ");     // print prompt
-
+        System.out.print("> ");
+     //返回回车键之前的所有信息
         inputLine = reader.nextLine();
-
-        Scanner tokenizer = new Scanner(inputLine);
+   //如果扫描到键入信息，分别保存两个分词后的信息
+        @SuppressWarnings("resource")
+		Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
-            word1 = tokenizer.next();      // get first word
+            word1 = tokenizer.next();   
             if(tokenizer.hasNext()) {
-                word2 = tokenizer.next();      // get second word
+                word2 = tokenizer.next();
             }
         }
-
-        Command command = commands.get(word1);
-        if(command != null) {
-            command.setSecondWord(word2);
+//对识别到的指令动作进行分析是否有效，
+        if(commands.isCommand(word1)) {
+            return new Command(word1, word2);
         }
-        return command;
+        else {
+            return new Command(null, word2);
+        }
     }
 
     public void showCommands()
@@ -44,6 +57,7 @@ public class Parser
         commands.showAll();
     }
 }
+
 
 
 
