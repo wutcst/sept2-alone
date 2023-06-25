1e03b45aebb60bc3343de66722a491728befd709
AWEI0407 <121768425+AWEI0407@users.noreply.github.com>
2023-06-25 13:25:10 +0800

    优化类

--- src/main/java/cn/edu/whut/sept/zuul/QuitCommand.java
+++ src/main/java/cn/edu/whut/sept/zuul/QuitCommand.java
@@ -1,16 +1,34 @@
-package cn.edu.whut.sept.zuul;
+package ZuulCommands;
 
-public class QuitCommand extends Command
+import Game.AGame;
+import Commands.ACommand;
+import Character.Player;
+
+/**
+ *此命令类是退出命令
+ * @author duoduo
+ *
+ */
+public class QuitCommand extends ACommand
 {
-    public boolean execute(Game game)
+    public QuitCommand(String firstWord, String secondWord, String thirdWord)
     {
-        if(hasSecondWord()) {
-            System.out.println("Quit what?");
+        super(firstWord, secondWord, thirdWord);
+    }
+    
+    public QuitCommand() {}
+    
+    /**
+     * quit命令的执行命令
+     */
+    @Override
+    public boolean execute(Player player)
+    {       
+        if (hasSecondWord())
+        {
+            AGame._out.println(AGame._messages.getString("quitWhat"));
             return false;
         }
-        else {
-            return true;  // signal that we want to quit
-        }
+        return true;
     }
 }
-
